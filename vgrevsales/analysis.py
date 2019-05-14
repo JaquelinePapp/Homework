38d6fc93baee8e1236853987e2f3c8545fa919a2
joshuamegnauth54 <joshua.megnauth54@gmail.com>
2019-05-14 06:08:29 -0400

    Added error handling to main. ECDF and comments added to analysis.py.

--- vgrevsales/analysis.py
+++ vgrevsales/analysis.py
@@ -19,10 +19,16 @@
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+from statsmodels.distributions.empirical_distribution import ECDF
 
 
 def review_sales_hexbins(vgsales):
     """
+    Creates and returns a hexbin plot of video game sales distributed over
+    Metacritic scores.
+
+    :param vgsales: DataFrame of the Video_Games_Sales_as_at_22_Dec_2016
+    dataset or equivalent.
     """
 # =============================================================================
 #     A hexbins plot was the most fruitful EDA for this data. Scores (x) and
@@ -49,12 +55,25 @@ def review_sales_hexbins(vgsales):
     return hexgraph
 
 
-def review_sales_hist(vgsales):
+def genre_sales_boxen(vgsales, sizex=10, sizey=10):
+    """
+    Creates and returns a boxen categorical plot of sales by video game genre.
+
+    :param vgsales: DataFrame of the Video_Games_Sales_as_at_22_Dec_2016
+    dataset or equivalent.
+
+    :param sizex: Size in inches(?) of the width of the plot.
+
+    :param sizey: Size in inches(?) of the height of the plot.
+    """
     gensales = sns.catplot("Genre", "Sales", data=vgsales, kind="boxen")
+
+    # We use a logarithmic y scale due to the range of y values.
+    # The figure size is debatable, but the plot looks weird when it's smaller.
 
     gensales.set_xticklabels(rotation=45)
     gensales.ax.set_yscale("log")
-    gensales.fig.set_size_inches(10, 10, forward=True)
+    gensales.fig.set_size_inches(sizex, sizey, forward=True)
 
     gensales.ax.set_title("Game sales distributed by genre")
     gensales.set_axis_labels(y_var="Global sales (millions)")
@@ -62,3 +81,22 @@ def review_sales_hist(vgsales):
     plt.show()
 
     return gensales
+
+
+def sales_review_ecdf(vgsales):
+    """
+    ECDF
+    """
+    rev_ecdf = ECDF(vgsales.Metacritic)
+    sales_ecdf = ECDF(vgsales.Sales)
+    fig, ax = plt.subplots()
+    ax.set_ylabel("ECDF")
+
+    sns.lineplot(rev_ecdf.x, rev_ecdf.y, ax=ax)
+    ax.set_xlabel("Review probability (0 to 100)")
+
+    twin_y = ax.twiny()
+    sns.lineplot(sales_ecdf.x, sales_ecdf.y, ax=twin_y)
+    twin_y.set_xlabel("Sales probability (millions)")
+
+    plt.show()
--- vgrevsales/main.py
+++ vgrevsales/main.py
@@ -15,18 +15,48 @@ from loadprep import year_range_vgsales
 from loadprep import set_seaborn_opts
 from descriptivestats import print_descriptive
 from analysis import review_sales_hexbins
+from analysis import genre_sales_boxen
 
 
 def parse_args():
     """
     """
-    varargs = ArgumentParser()
 
-    varargs.add_argument("-vgsales", help="Path to a dataset compatible with "
-                         "Video_Games_Sales_as_at_22_Dec_2016.csv")
+    varargsparser = ArgumentParser()
+    varargsparser.add_argument("-vgsales",
+                               help="Path to a dataset compatible with "
+                               "Video_Games_Sales_as_at_22_Dec_2016.csv")
 
-    return varargs.parse_args()
+    # Set a default path if vgsales is null
+    varargs = varargsparser.parse_args()
+
+    if not varargs.vgsales:
+        varargs.vgsales = "Video_Games_Sales_as_at_22_Dec_2016.csv"
+
+    return varargs
 
 
-if __name__ == "__MAIN__":
-    vgsales = loadprep_vgsales(parse_args().vgsales)
+def main_error_handler(e, path):
+    print("Error: Unable to load the required dataset. Tried: {0}"
+          .format(path))
+
+    print("Python error: {0}".format(e))
+
+
+if __name__ == "__main__":
+    vgsales = None
+
+    try:
+        arguments = parse_args()
+        vgsales = loadprep_vgsales(arguments.vgsales)
+    except ValueError as e:
+        main_error_handler(e, arguments.vgsales)
+    except FileNotFoundError as e:
+        main_error_handler(e, arguments.vgsales)
+
+    # Now that that's out of the way, we can run all of our projects' functions
+    # here.
+    set_seaborn_opts()
+    print_descriptive(vgsales)
+    review_sales_hexbins(vgsales)
+    genre_sales_boxen(vgsales)
