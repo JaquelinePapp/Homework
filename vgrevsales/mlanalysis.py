bbba6309500372c4a602786ceb81b856eb0336c8
JaquelinePapp <49172723+JaquelinePapp@users.noreply.github.com>
2019-05-18 21:17:45 -0400

    Update mlanalysis.py

--- vgrevsales/mlanalysis.py
+++ vgrevsales/mlanalysis.py
@@ -8,6 +8,9 @@
 # (Working) Project Title: Do review scores affect video game sales?
 # =============================================================================
 
+import numpy as np #for arrays
+import matplotlib.pyplot as plt #for visualization
+
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.cluster import KMeans
@@ -29,3 +32,21 @@ def pre_preprocess_features(vgsales):
 def preprocess_vgsales(vgsales):
     for x in range(1984, 2017):
         vgsales[vgsales.Release == x].info()
+        
+        
+def linear_regression(vgsales):
+
+    reg = LinearRegression()
+    X = vgsales["Metacritic"].values.reshape(-1, 1)  
+    Y = vgsales["Sales"].values.reshape(-1, 1)
+    reg.fit(X, Y)  #perform linear regression
+    Y_pred = reg.predict(X)
+
+
+    scatter = plt.scatter(X, Y)
+    plt.title('Linear Regression')
+    plt.yscale('log')
+    plt.plot(X, Y_pred, color='red')
+    plt.show()
+
+    return scatter
